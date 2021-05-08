c196c9bdbd2f265bf8746842c024f1ee84a5a9a5
Dinesh Sonachalam <dineshsonachalam@gmail.com>
2021-05-08 22:24:29 +0530

    CI/CD test

---
+++ test1-mysql.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.pool import NullPool
+from sqlalchemy.orm import sessionmaker
+import json
+
+SQLALCHEMY_DATABASE_URL = "mysql+mysqlconnector://root:simple@127.0.0.1:3306/adp"
+
+if __name__ == "__main__":
+    # Define the MySQL engine using MySQL Connector/Python
+    db = create_engine(SQLALCHEMY_DATABASE_URL, poolclass=NullPool)
+    metadata = MetaData()
+
+    battles = Table('battles', metadata, autoload=True, autoload_with=db)
+
+    session = sessionmaker(bind=db)
+    session = session()
+
+    battle_info = session.query(battles).with_entities(battles.c.attacker_king, battles.c.defender_king,
+                                                        battles.c.attacker_outcome, battles.c.year).all() 
+
+    print("Battle info: ",battle_info)
